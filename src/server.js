a10cd0b36a649b7047d402f7b14968ec52855bb9
Snoozeds <88577069+Snoozeds@users.noreply.github.com>
2025-09-23 18:28:13 +0100

    make server.js actually use PORT env variable

--- src/server.js
+++ src/server.js
@@ -56,6 +56,6 @@ const server = http.createServer((req, res) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("Server running at http://localhost:3000");
+server.listen(process.env.PORT ?? 3000, () => {
+  console.log(`Server running at http://localhost:${process.env.PORT ?? 3000}`);
 });
